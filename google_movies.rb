a12c45dee0a1e814fe9722ae3232f568b7a78d41
Apprentice <apprentice@StMac3-3.local>
2015-11-06 13:16:32 +1300

    scraper for google movies, scrape title and cinema, then put into multi-dimensional hash

---
+++ google_movies.rb
@@ -0,0 +1,36 @@
+require 'mechanize'
+
+class GoogleMovies
+  def initialize
+    @base_url = "https://www.google.com/movies?near=wellington,&sort=1"
+    @Movies = {}
+  end
+
+  def getStuff
+    found_movies = []
+    dates = []
+    agent = Mechanize.new { |agent|
+      agent.user_agent_alias = 'Windows Chrome'
+    }
+    #get titles of movies
+    agent.get(@base_url) { |page|
+      page.links_with(:href => /movies\?near=wellington,\&sort=1&mid=/).each do |link|
+        description_page = link.click
+        title = description_page.search('h2[itemprop="name"]').text.strip
+        @Movies[title] = {}
+        description_page.search('.theater .name').each do |cinema|
+          @Movies[title] = {cinema.text.strip => []}
+        end
+      end
+      # @Movies.each do |movie|
+      #   p movie
+      # end
+      p @Movies
+    }
+  end
+end
+
+google_movies = GoogleMovies.new
+google_movies.getStuff
+
+
